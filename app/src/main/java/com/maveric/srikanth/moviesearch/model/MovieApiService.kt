94414cde9ac61eb73a05a6a3bec54ca60fa4c905
srikanth-maveric <srikanthpr@maveric-systems.com>
2020-06-22 16:51:51 +0530

    Improved API level code

--- app/src/main/java/com/maveric/srikanth/moviesearch/model/MovieApiService.kt
+++ app/src/main/java/com/maveric/srikanth/moviesearch/model/MovieApiService.kt
@@ -1,30 +1,31 @@
 package com.maveric.srikanth.moviesearch.model
 
 import io.reactivex.Single
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MovieApiService {
 
-    private val apiKey = "b9bd48a6"
-    private val baseUrl = "http://www.omdbapi.com/?apikey=b9bd48a6"
+    private companion object {
+        const val API_KEY = "b9bd48a6"
+        const val BASE_URL = "http://www.omdbapi.com/"
+        const val TYPE = "movie"
+    }
 
 
     private val api = Retrofit.Builder()
-        .baseUrl(baseUrl)
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
         .create(MoviesApi::class.java)
 
     fun getMovies(movieTitle: String, pageNumber: Int): Single<MovieListResponse> {
-        return api.getMovieList(apiKey, movieTitle, "movie", pageNumber)
+        return api.getMovieList(API_KEY, movieTitle, TYPE, pageNumber)
     }
 
     fun getMovieDetail(movieId: String): Single<MovieDetailResponse> {
-        return api.getMovieDetail(apiKey, movieId)
+        return api.getMovieDetail(API_KEY, movieId)
     }
 }
