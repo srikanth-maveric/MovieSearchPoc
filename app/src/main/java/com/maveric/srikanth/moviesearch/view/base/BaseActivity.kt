8d465739699f4cd894062f77862fcd5bced912a9
srikanth-maveric <srikanthpr@maveric-systems.com>
2020-06-24 14:10:48 +0530

    Removed redundant code

--- app/src/main/java/com/maveric/srikanth/moviesearch/view/base/BaseActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.maveric.srikanth.moviesearch.view.base
-
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-
-class BaseActivity: AppCompatActivity() {
-    protected var navController: NavController? = null
-
-}
